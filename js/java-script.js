2993ecc151aab490681fdde713d838c817912d40
ahlamalefishat96 <ahlam.alefishat@gmail.com>
2020-02-05 00:10:36 +0200

    javascript is ready

---
+++ js/java-script.js
@@ -0,0 +1,31 @@
+var Name= prompt ('What is your name?');
+document.write ('<h1> Hello '+Name+' </h1>');
+
+
+
+
+var  code = prompt (' Type yes if you like coding , and no if you dont');
+console.log(code);
+
+var greeting; 
+if (code= "yes" ){
+    greeting='Your fav language :';
+    
+document.write ('<h2> '+greeting+' </h2>')
+
+var lan= prompt ('What is your fav language?');
+document.write ('<h1>  '+lan+' </h1>');
+}
+
+
+else {
+    greeting = 'something went wrong';
+}
+
+
+
+var age= prompt ('How old are you?');
+document.write ('<h1>  '+age+' </h1>');
+
+
+
